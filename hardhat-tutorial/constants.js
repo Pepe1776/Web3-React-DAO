1153e1af90e15c2296399d3bf0bd21a8b3971b22
J Not Query <93418359+Pepe1776@users.noreply.github.com>
2022-03-30 19:59:54 -0700

    iniitial

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0xE5e587af70e4f4Fd5FD5765DFfb6D5AAe776a8B2";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
